b3c1b2f00924ac6646004c01fcdc29f9464711ac
Jordan Hart <p22jhart@fairfieldprep.org>
2021-11-09 07:19:40 -0800

    Hw 6-1

---
+++ hw-6-2.py
@@ -0,0 +1,20 @@
+# JH 11/9/21
+
+my_list = [ '1', '2', '3','4','5','6','7']
+
+print(len(my_list))
+
+print(5 in(my_list))
+
+print(10 not in(my_list))
+
+name_list=['Jason','Jordan','Mohammed']
+
+print(name_list[1])
+
+
+print(name_list*3)
+
+print(my_list+name_list)
+
+print(len(my_list+name_list))
